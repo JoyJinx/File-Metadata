f4ae6240db426f9276ddf2c070f84c0960f77db6
CodeSandbox <support@codesandbox.io>
2023-11-14 13:39:37 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 100 * 1024,
+    files: 1, 
+    fields: 1,
+  },
+});
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.post('/api/fileanalyse', upload.single("upfile"), function(req, res){
+  console.log(req.file)
+  const metadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+  console.log(metadata);
+  return res.json(metadata);
+})
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
